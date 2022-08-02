685e1858f45558b860ccec122512b5837eed60b6
Richard Chu <rchu1211@gmail.com>
2022-08-02 07:59:49 -0700

    controllers

--- src/main/java/com/codingdojo/firstproject/FirstprojectApplication.java
+++ src/main/java/com/codingdojo/firstproject/FirstprojectApplication.java
@@ -1,9 +1,11 @@
 package com.codingdojo.firstproject;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @SpringBootApplication
 @RestController
@@ -13,8 +15,8 @@ public class FirstprojectApplication {
 		SpringApplication.run(FirstprojectApplication.class, args);
 	}
 
-	@RequestMapping("/")
-	public String hello() {
-		return "Hello World!!!";
-	}
+//	@RequestMapping("/")
+//	public String hello() {
+//		return "Hello World!!!";
+//	}
 }
